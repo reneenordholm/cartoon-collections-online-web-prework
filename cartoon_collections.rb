517f49f81b648abfb0f4f245c140018a36966389
Renee Nordholm <reneenordholm@gmail.com>
2019-03-30 23:53:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies
+  veggies.collect { |veggie| veggies.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
@@ -14,3 +14,10 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+#names = ['danil', 'edmund']
+#names.map! {|name| name.capitalize }
+
+#a = [ "a", "b", "c", "d" ]
+#a.collect {|x| x + "!"}   
