57ef9f21d408f813db271753b489c21d860fb535
Renee Nordholm <reneenordholm@gmail.com>
2019-03-30 23:54:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.collect { |veggie| veggies.capitalize + "!" }
+  veggies.collect { |veggie| veggies.upcase + "!" }
 end
 
 def long_planeteer_calls# code an argument here
