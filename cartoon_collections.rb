4743ac387729ec6f86e1777d720c198ee36e7851
Renee Nordholm <reneenordholm@gmail.com>
2019-03-31 00:01:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
  dwarves.each_with_index { |dwarves, i| puts "#{i+1}. #{dwarves}" }
 end
 
-def summon_captain_planet(fruits, veggies)
+def summon_captain_planet(veggies)
   veggies.map! {|veggies| veggies.capitalize }
   veggies.each {|veggies| puts veggies + "!" }
 end
