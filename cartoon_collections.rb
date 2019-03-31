6000236283fad003e8e8956585533e829da50b54
Renee Nordholm <reneenordholm@gmail.com>
2019-03-31 00:14:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese(cheddar_cheese)
   if cheddar_cheese.include?("cheddar")
-    return cheddar_cheese
+    return cheddar
   else
     nil
   # the array below is here to help
